8de9ee57c0b649613eb248a86f81a03e51b32601
Trevin Chow <trevin@trevinchow.com>
2024-01-19 15:52:18 -0800

    fix: fix currency formatting for various locales

--- spec/integration/currency_locales_spec.rb
+++ spec/integration/currency_locales_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 shared_examples_for "locale" do |locale_code, small_currency, big_currency, small_precision, big_precision|
   let(:locale) { locale_code }
 
-  it "should translate the small amount to currency" do
+  it "should translate the small amount to currency for locale #{locale_code}" do
     expect(number_to_currency(small_amount)).to eq(small_currency)
   end
 
-  it "should translate the big amount to currency" do
+  it "should translate the big amount to currency for locale #{locale_code}" do
     expect(number_to_currency(big_amount)).to eq(big_currency)
   end
 
-  it "should translate the small amount with precision" do
+  it "should translate the small amount with precision for locale #{locale_code}" do
     expect(number_with_precision(small_amount)).to eq(small_precision)
   end
 
-  it "should translate the big amount with precision" do
+  it "should translate the big amount with precision for locale #{locale_code}" do
     expect(number_with_precision(big_amount)).to eq(big_precision)
   end
 end
@@ -34,36 +34,33 @@ describe "CurrencyLocales" do
 
   locales = {
     'cs' => ['9,99Kč', '9999,99Kč', '9,99', '9999,99'],
-    'da' => ['kr9,99', 'kr9.999,99', '9,99', '9.999,99'],
+    'da' => ['9,99kr', '9.999,99kr', '9,99', '9.999,99'],
     'en-AU' => ['$9.99', '$9,999.99', '9.99', '9,999.99'],
     'en-GB' => ['£9.99', '£9,999.99', '9.99', '9,999.99'],
-    'en-IN' => ['₹9.99', '₹9,999.99', '9.99', '9,999.99'],
+    'en-IN' => ['₹9.99', '₹9,999.99', '9.99', '9999.99'],
     'en-PH' => ['₱9.99', '₱9,999.99', '9.99', '9,999.99'],
-    'es-MX' => ['$9.99', '$9,999.99', '9.99', '9,999.99'],
     'en-US' => ['$9.99', '$9,999.99', '9.99', '9,999.99'],
+    'es-MX' => ['$9.99', '$9,999.99', '9.99', '9,999.99'],
     'eu' => ['€9.99', '€9,999.99', '9.99', '9,999.99'],
     'gsw-CH' => ['CHF9.99', 'CHF9,999.99', '9.99', '9,999.99'],
     'hu' => ['10Ft', '10000Ft', '9,99', '9999,99'],
-    'id' => ['Rp9,99', 'Rp9.999,99', '9,99', '9.999,99'],
-    'it' => ['€9,99', '€9.999,99', '9,99', '9.999,99'],
-    'ko' => ['₩9.99', '₩9,999.99', '9.99', '9,999.99'],
-    'nl' => ['€9,99', '€9.999,99', '9,99', '9.999,99'],
-    'pt' => ['R$9,99', 'R$9.999,99', '9,99', '9.999,99'],
-    'ro' => ['9,99Lei', '9.999,99Lei', '9,99', '9.999,99'],
-    'ru' => ['9,99р.', '9.999,99р.', '9,99', '9.999,99'],
-    'sk' => ['9,99€', '9.999,99€', '9,99', '9.999,99'],
+    'il' => ['₪9.99', '₪9,999.99', '9.99', '9,999.99'],
+    'ja' => ['¥10', '¥10,000', '9.99', '9,999.99'],
+    'ms-MY' => ['RM9.99', 'RM9,999.99', '9.99', '9,999.99'],
+    'nb' => ['9,99 kr', '9999,99 kr', '9,99', '9999,99'],
+    'pl' => ['9,99 zł', '9999,99 zł', '9,99', '9999,99'],
+    'pt-BR' => ['R$9,99', 'R$9.999,99', '9,99', '9.999,99'],
+    'sv-SE' => ['9,99 kr', '9999,99 kr', '9,99', '9999,99'],
     'th' => ['฿9.99', '฿9,999.99', '9.99', '9,999.99'],
-    'tr' => ['9,99₺', '9.999,99₺', '9,99', '9.999,99'],
-    'uk' => ['9,99₴', '9.999,99₴', '9,99', '9.999,99'],
-    'vi' => ['9,99₫', '9.999,99₫', '9,99', '9.999,99'],
-    'zh-CN' => ['¥9.99', '¥9,999.99', '9.99', '9,999.99'],
-    'zh-HK' => ['HK$9.99', 'HK$9,999.99', '9.99', '9,999.99'],
-    'zh-SG' => ['S$9.99', 'S$9,999.99', '9.99', '9,999.99'],
+    'tr' => ['₺9,99', '₺9.999,99', '9,99', '9.999,99'],
     'zh-TW' => ['NT$9.99', 'NT$9,999.99', '9.99', '9,999.99'],
   }
 
-  locales.each do |locale, values|
-    it_behaves_like "locale", locale, *values
+  sorted_locales = locales.sort_by { |locale, _| locale }.to_h
+
+  sorted_locales.each do |locale, values|
+    describe "locale #{locale}" do
+        it_behaves_like "locale", locale, *values
+    end
   end
 end
-
